3018dbdac9648d7149c78c0ffe4d96333e9e9caa
MitsukiIwamura <boc.fsfb@gmail.com>
2022-09-22 21:07:00 +0900

    first commit

---
+++ libs/client.ts
@@ -0,0 +1,7 @@
+import { env } from 'process'
+import { createClient } from 'microcms-js-sdk'
+
+export const client = createClient({
+    serviceDomain: process.env.DOMAIN || '',
+    apiKey: process.env.API_KEY || '',
+})
